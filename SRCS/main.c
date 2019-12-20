4bbccb114007580e939af298aa41db9c771a9ba4
Brandon Feltz <elloimbrandon@gmail.com>
2019-12-20 00:43:30 -0800

    .

--- SRCS/main.c
+++ SRCS/main.c
@@ -6,23 +6,20 @@
 /*   By: brandonf <brfeltz@student.42.us.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 17:16:50 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/12/18 23:16:20 by brandonf         ###   ########.fr       */
+/*   Updated: 2019/12/19 23:00:10 by brandonf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../HEADERS/ft_minishell.h"
 
+void    display_prompt()
+{
+    
+}
 
 /* Signal Handler for SIGINT */
 void sigint_handler(int sig_num)
-{
-    /* Reset handler to catch SIGINT next time.
-       Refer http://en.cppreference.com/w/c/program/signal */
-    printf("\n User provided signal handler for Ctrl+C \n");
-
-    /*
-    ** cleanup of the program like: free memory and exit
-    */ 
+{ 
     exit(0);
 }
 
@@ -31,7 +28,7 @@ int		main(int argc, char **argv)
     t_info *info;
 
     info = (t_info*)ft_memalloc(sizeof(t_info));
-    signal(SIGINT, sigint_handler);
+    signal(SIGINT, sigint_handler); // consider inside while loop.
     while(1)
     {
 
