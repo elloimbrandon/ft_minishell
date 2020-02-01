9979e588137b749fd407c100d7c4e26ff9c33406
elloimbrandon <elloimbrandon@gmail.com>
2020-01-31 21:46:55 -0800

    done

--- SRCS/ft_exec.c
+++ SRCS/ft_exec.c
@@ -6,13 +6,13 @@
 /*   By: brfeltz <brfeltz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 19:52:35 by brfeltz           #+#    #+#             */
-/*   Updated: 2020/01/31 20:06:35 by brfeltz          ###   ########.fr       */
+/*   Updated: 2020/01/31 21:45:08 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../HEADERS/ft_minishell.h"
 
-void			ft_already_exc(t_cmd *input_check, char **input_copy)
+void	ft_already_exc(t_cmd *input_check, char **input_copy)
 {
 	if (ft_strcmp(input_copy[0], "echo") == 0)
 		input_check->executed = 1;
@@ -32,34 +32,34 @@ void			ft_already_exc(t_cmd *input_check, char **input_copy)
 		input_check->executed = 1;
 }
 
-void			ft_local_exec(char **input_copy, t_cmd *input_check, t_env *env)
+void	ft_local_exec(char **input_copy, t_cmd *input_check, t_env *env)
 {
 	char		*exec;
 	struct stat buf;
 
-	if (ft_strchr(input_copy[0], '/') != NULL && ft_strchr(input_copy[0], '.') != NULL)
+	if (ft_strchr(input_copy[0], '/') != NULL &&
+		ft_strchr(input_copy[0], '.') != NULL)
 	{
 		if (lstat(input_copy[0], &buf) != -1)
 		{
-			if S_ISREG(buf.st_mode)
+			if (access(input_copy[0], X_OK) == 0)
 			{
-				if (access(input_copy[0], X_OK) == 0)
-				{
-					exec = ft_strdup(input_copy[0]);
-					ft_fork(exec, input_copy, env);
-				}
-				else
-					ft_printf("%sshell: Permission denied: %s\n", KRED, input_copy[0]);
+				exec = ft_strdup(input_copy[0]);
+				ft_fork(exec, input_copy, env);
 			}
+			else
+				ft_printf("%sshell: Permission denied: %s\n", KRED,
+					input_copy[0]);
 			chdir(input_copy[0]);
 		}
 		else
-			ft_printf("%sshell: no such file or directory: %s\n", KRED, input_copy[0]);
+			ft_printf("%sshell: no such file or directory: %s\n", KRED,
+				input_copy[0]);
 		input_check->executed = 1;
 	}
 }
 
-void			check_system_cmd(char **input_copy, t_env *env)
+void	check_system_cmd(char **input_copy, t_env *env)
 {
 	char		*hold;
 
@@ -71,7 +71,7 @@ void			check_system_cmd(char **input_copy, t_env *env)
 	free(hold);
 }
 
-void			check_exec(char *hold, char **input_copy, t_env *env)
+void	check_exec(char *hold, char **input_copy, t_env *env)
 {
 	char		**path;
 	int			k;
@@ -83,7 +83,8 @@ void			check_exec(char *hold, char **input_copy, t_env *env)
 		ft_printf("%sshell: command not found: %s\n", KRED, input_copy[0]);
 }
 
-int				check_path_build_path(char **path, char **input_copy, t_env *env)
+int		check_path_build_path(char **path,
+						char **input_copy, t_env *env)
 {
 	int			i;
 	int			k;
@@ -92,7 +93,7 @@ int				check_path_build_path(char **path, char **input_copy, t_env *env)
 
 	i = 0;
 	k = 0;
-	while(path[++i])
+	while (path[++i])
 	{
 		exec = build_path(input_copy[0], path[i]);
 		if (lstat(exec, &buf) != 1)
